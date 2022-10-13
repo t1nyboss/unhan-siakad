d119fca12c41f0bc12cd36f24ab1acaa835ce3f1
navalinovian <viansatria57@gmail.com>
2022-10-13 23:59:01 +0700

    sequelize init, connect backend

--- backend/app.js
+++ backend/app.js
@@ -1,11 +1,22 @@
 const express = require('express')
 const app = express()
 const port = 3000
+// const db = require('./queries.js')
+var bodyParser = require('body-parser')
+const cors = require("cors");
+const { sequelize } = require('./models')
 
+// const allroute = require('./router')
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
   res.send('Hello World!')
 })
+// app.use('/api', allroute );
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+app.listen(port, async () => {
+  console.log(`Un app listening on port ${port}`)
+  await sequelize.authenticate()
+  console.log('Database Connected!');
 })
